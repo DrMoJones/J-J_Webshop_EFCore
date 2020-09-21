7e125584c7a21126e2b25426a0c72eeacad000f7
Jakob Vestergaard <jakobmetalv@gmail.com>
2020-09-21 12:13:45 +0200

    Setup init

---
+++ Webshop.Data/WebshopContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Webshop.Data
+{
+    public class WebshopContext : DbContext
+    {
+
+    }
+}
---
+++ Webshop.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Webshop.Domain
+{
+    public class Class1
+    {
+    }
+}
